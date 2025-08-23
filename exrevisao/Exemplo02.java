69b2dc191fd8559d061447a7f8f19069a89bdf66
Carlos Henrique da Costa Souza <carloshcsouza27@GMail.com>
2025-08-23 00:31:45 +0000

    Sequencias for

---
+++ exrevisao/Exemplo02.java
@@ -0,0 +1,28 @@
+public class Exemplo02 { 
+    public static void main(String[] args) {
+        System.out.println("Olá, Turma!");
+
+        //for (int i = 1; i <=100; i++) {
+            //System.out.print(i + " ");
+        //}
+
+         //for (int i = 1; i <100; i+=2) {
+            //System.out.print(i + " ");
+        
+        
+         //for (int i = 2; i <=100; i += 2) {
+            //System.out.print(i + " ");
+        //}
+        
+
+         //for (int i = 3; i <= 99; i += 3) {
+            //System.out.print(i + " ");
+        //}
+
+         for (int i = 1; i <100; i+=2) {
+            if (i % 3 == 0 ||i == 13||i == 23);
+            System.out.print(i + " ");
+        }
+
+}
+}
